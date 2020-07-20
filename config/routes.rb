fdda14bb79e3028c5e1b87f06956d1e2bfc7c011
ashna shah <a5hna@hotmail.co.uk>
2020-07-20 18:44:18 +0100

    added race blog post

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'home', to: 'pages#home'
   get 'publications', to: 'pages#publications'
+  get 'race', to: 'pages#race'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
